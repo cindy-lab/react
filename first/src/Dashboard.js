faa0d7715ac25322ee84ec9b0884c59ea7e25883
thevalak <diansinj@gmail.com>
2019-10-20 17:39:10 +0800

    Third Commit

--- first/src/Dashboard.js
+++ first/src/Dashboard.js
@@ -1,16 +1,42 @@
 import React, {Component}  from 'react'
+import './Dashboard.scss'
 
 class Dashboard extends Component{
     constructor(props){
         super(props);
         this.state ={
-
+            pic: [],
+            num: 0
         }
+        this.addPic = this.addPic.bind(this)
     }
-}
-render() {
+    addPic =(e)=>{
+        this.setState({pic: URL.createObjectURL(e.target.files[this.state.num])})
+    }
+    render() {
+        const{pic}=this.state;
     return(
-        <div>Welcome to Stargaze</div>
-    )
+        <div>
+            <h1>Welcome to Stargaze</h1>
+            <div id="profiles">
+                <h2>Profile</h2>
+                <h2>Timeline</h2>
+                <h2>Gallery</h2>
+                <h2>Setting</h2>
+            </div>
+            <div id="starfeed">
+                <form action="/action_page.php">
+                    <input type="file"  accept="file_extension|video/*|image/*" onChange={this.addPic}></input>
+                    <br/><img src={pic}></img>
+                </form>
+            </div>
+            <div id="notification">
+            <h1>Notification</h1>
+            </div>
+            
+            
+        </div>        
+    );
 };
+}
 export default Dashboard
