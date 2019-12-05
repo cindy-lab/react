999f278ece2831a752d77034969db9181ebb87e0
cindy-lab <54087806+cindy-lab@users.noreply.github.com>
2019-12-05 20:37:19 +0800

    not final

--- first/src/component/ImageUpload.js
+++ first/src/component/ImageUpload.js
@@ -1,78 +1,107 @@
 import React from 'react';
 import PostAddIcon from '@material-ui/icons/PostAdd';
 import Typography from '@material-ui/core/Typography';
-import axios from 'axios';import { Form, Input, Card, Button, Icon } from 'semantic-ui-react';
+import axios from 'axios';
+import { Form, Input, Card, Button, Icon } from 'semantic-ui-react';
+import fs from 'fs';
+const base = 'http://localhost:4000/uploads/';
 
 export default class ImageUpload extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {file: "",imageDescription: ""};
-    }  
-    
-    _handleSubmit(e) {
-      // e.preventDefault();
-      // // TODO: do something with -> this.state.file
-      // console.log('handle uploading-', this.state.file);
-      e.preventDefault();
-      var data = new FormData();
-      data.append('img', this.state.file);
-  
-      axios.post(`http://localhost:4000/uploads`,
-          data
-        ).then(res => {
-          console.log(res);
-        })
-        .catch(error => {
-          console.error("file upload failed", error);
-        });
-  
-      // TODO: do something with -> this.state.file
-      console.log('handle uploading-', this.state.file);
-    
-    }
-  
-    _handleImageChange(e) {
-      e.preventDefault();
-  
-      let reader = new FileReader();
-      let file = e.target.files[0];
-  
-      reader.onloadend = () => {
-        this.setState({
-          file: file,
-          imagePreviewUrl: reader.result
-        });
-      }
-  
-      reader.readAsDataURL(file)
-    }  
-    onChange = (e) => {
-      this.setState({imageDescription : e.target.value});
-    }
-    render() {
-      return (
-       <div>
-          <Form.TextArea placeholder='What is new with Star?'
-                                onChange={e => this.setState({ imageDescription: e.target.value })}
-                                style={{
-                                    width: 500,
-                                    height: 60,
-                                    fontSize: 20,
-                                    marginTop: 35
-                                }}
-                                // required
-                            />
-          <div>
-            <input className="PhotoInput" 
-              type="file" 
-              name="img"
-              onChange={(e)=>this._handleImageChange(e)} />
-            <button className="submitButton" 
-              onClick={(e)=>this._handleSubmit(e)}>Upload Image</button>
-          </div>
-        </div>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: null,
+      imageDescription: ""
+    };
   }
-    
- 
+  // onSubmit = (e) => {
+  //   e.preventDefault();
+
+  //   if(!this.state.file.files){
+  //     var file = fs.readFileSync(this.state.file.path);
+  //     console.log(file);
+
+  //   }
+  //   let url = 'http://localhost:4000/' + this.state.file.filename;
+  //   const data = {
+  //     userId : this.state.user._id,
+  //     imageDescription : this.state.imageDescription,
+  //     imageURL: url,
+  //     imageData : req.file.path,
+  //     created_at:new Date()
+  //   };
+  //   axios.post(`${base}uploadmulter`, data)
+  //     .then((response) => {
+  //       alert("The file is successfully uploaded");
+  //     }).catch((error) => {
+
+  //     });
+
+  // }
+  _handleSubmit(e) {
+    // e.preventDefault();
+    // // TODO: do something with -> this.state.file
+    // console.log('handle uploading-', this.state.file);
+    e.preventDefault();
+    var data = new FormData();
+    data.append('img', this.state.file);
+
+    axios.post(`http://localhost:4000/uploads`,
+      data
+    ).then(res => {
+      console.log(res);
+    })
+      .catch(error => {
+        console.error("file upload failed", error);
+      });
+
+    // TODO: do something with -> this.state.file
+    console.log('handle uploading-', this.state.file);
+
+  }
+
+  _handleImageChange(e) {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    }
+
+    reader.readAsDataURL(file)
+  }
+  onChange = (e) => {
+    this.setState({ imageDescription: e.target.value });
+  }
+  render() {
+    return (
+      <div>
+        <Form.TextArea placeholder='What is new with Star?'
+          onChange={e => this.setState({ imageDescription: e.target.value })}
+          style={{
+            width: 500,
+            height: 60,
+            fontSize: 20,
+            marginTop: 35
+          }}
+        // required
+        />
+        <div>
+          <input className="PhotoInput"
+            type="file"
+            name="img"
+            onChange={(e) => this._handleImageChange(e)}
+          />
+          <button className="submitButton"
+            onClick={(e) => this._handleSubmit(e)}>Upload Image</button>
+        </div>
+      </div>
+    );
+  }
+}
+
